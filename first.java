7cb64b2f2eb1a59d853355e477503fa052e637f8
Vickey <vickyhatagale505@gmail.com>
2023-03-29 17:49:05 +0530

    second commit

--- first.java
+++ first.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 class Test{
     public static void main(String args[]){
         System.out.println("my first git repo");
+         System.out.println("enter the first  values");
+         System.out.println("enter the second values")
     }
-}
+} 
