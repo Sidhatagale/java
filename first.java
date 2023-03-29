8ca49b4f42ab1c820eb9b9f7eeb0195f16c44200
Vickey <vickyhatagale505@gmail.com>
2023-03-29 18:03:47 +0530

    first commit

--- first.java
+++ first.java
@@ -3,6 +3,7 @@ class Test{
     public static void main(String args[]){
         System.out.println("my first git repo");
          System.out.println("enter the first  values");
-         System.out.println("enter the second values")
+         System.out.println("enter the second values");
+         System.out.println("for first branch");
     }
 } 
